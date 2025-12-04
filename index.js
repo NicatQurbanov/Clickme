48d0681f2e812794069a60ee1d817b978daedd72
NicatQurbanov <nicatqur1307@gmail.com>
2025-12-04 17:42:54 +0400

    to be continued

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const firstSecform1 = document.getElementById('firstSecform1')
 const firstSecform2 = document.getElementById('firstSecform2')
 const secondSecform1 = document.getElementById('secondSecform1')
 const secondSecform2 = document.getElementById('secondSecform2')
+const main = document.getElementById('main')
 
 btnSignUp.addEventListener('click', e => {
   firstSecform1.style.transform = 'translateY(0%)';
   firstSecform2.style.transform = 'translateY(-100%)';
   secondSecform1.style.transform = 'translateY(0%)';
   secondSecform2.style.transform = 'translateY(-100%)';
-  const firstSecElements = firstSec.querySelectorAll('.defaultOpacity');
+  
+  
+  const firstSecElements = main.querySelectorAll('.defaultOpacity');
   for (let tag of firstSecElements) {
   tag.classList.add('opacity');
   tag.classList.remove('defaultOpacity');
   tag.classList.add('transition');
-  const secondSecElements = secondSec.querySelectorAll('.opacity');
+  const secondSecElements =  main.querySelectorAll('.opacity');
   for (let tag of secondSecElements) {
    tag.classList.add('defaultOpacity');
    tag.classList.remove('opacity');
@@ -32,12 +35,12 @@ btnSignIn.addEventListener('click', e => {
   secondSecform2.style.transform = 'translateY(0%)';
   firstSecform1.style.transform = 'translateY(-100%)';
   secondSecform1.style.transform = 'translateY(-100%)';
-  const secondSecElements = secondSec.querySelectorAll('.defaultOpacity');
+  const secondSecElements = main.querySelectorAll('.defaultOpacity');
   for (let tag of secondSecElements) {
    tag.classList.remove('defaultOpacity');
    tag.classList.add('opacity');
   }
-  const firstSecElements = firstSec.querySelectorAll('.opacity');
+  const firstSecElements = main.querySelectorAll('.opacity');
   for (let tag of firstSecElements) {
     tag.classList.remove('opacity');
     tag.classList.add('defaultOpacity');
@@ -45,3 +48,16 @@ btnSignIn.addEventListener('click', e => {
 })
 
 
+const arr = document.querySelectorAll('body *')
+
+for (let i = 0; i < arr.length; i++) {
+ if (arr[i].classList.contains('opacity')){
+  console.log(arr[i])
+ }
+}
+
+for (let i = 0; i < arr.length; i++) {
+ if (arr[i].classList.contains('defaultOpacity')){
+  console.log(arr[i])
+ }
+}
