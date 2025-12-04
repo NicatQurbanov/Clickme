d8b97b3b86becec420471c0d5d1038fc5f4b5732
NicatQurbanov <nicatqur1307@gmail.com>
2025-12-04 17:23:33 +0400

    to be continued

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const secondSec = document.getElementById('secondSec');
 const btnSignUp = document.getElementById('sign-up');
 const btnSignIn = document.getElementById('sign-in');
 const opacity= document.getElementsByClassName('defaultOpacity');
+const firstSecform1 = document.getElementById('firstSecform1')
+const firstSecform2 = document.getElementById('firstSecform2')
+const secondSecform1 = document.getElementById('secondSecform1')
+const secondSecform2 = document.getElementById('secondSecform2')
 
 btnSignUp.addEventListener('click', e => {
-  firstSec.style.transform = 'translateY(-100%)';
-  secondSec.style.transform = 'translateY(-100%)';
+  firstSecform1.style.transform = 'translateY(0%)';
+  firstSecform2.style.transform = 'translateY(-100%)';
+  secondSecform1.style.transform = 'translateY(0%)';
+  secondSecform2.style.transform = 'translateY(-100%)';
   const firstSecElements = firstSec.querySelectorAll('.defaultOpacity');
   for (let tag of firstSecElements) {
   tag.classList.add('opacity');
@@ -22,8 +28,10 @@ btnSignUp.addEventListener('click', e => {
 })
 
 btnSignIn.addEventListener('click', e => {
-  firstSec.style.transform = 'translateY(0%)';
-  secondSec.style.transform = 'translateY(0%)';
+  firstSecform2.style.transform = 'translateY(0%)';
+  secondSecform2.style.transform = 'translateY(0%)';
+  firstSecform1.style.transform = 'translateY(-100%)';
+  secondSecform1.style.transform = 'translateY(-100%)';
   const secondSecElements = secondSec.querySelectorAll('.defaultOpacity');
   for (let tag of secondSecElements) {
    tag.classList.remove('defaultOpacity');
