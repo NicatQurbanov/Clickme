8f4a3ea4cd83ecd0c7737ed16a1054b4e47ff79b
NicatQurbanov <nicatqur1307@gmail.com>
2025-12-05 10:08:47 +0400

    to be continued

--- index.js
+++ index.js
@@ -16,18 +16,10 @@ btnSignUp.addEventListener('click', e => {
   firstSecform2.style.transform = 'translateY(-100%)';
   secondSecform1.style.transform = 'translateY(0%)';
   secondSecform2.style.transform = 'translateY(-100%)';
-  
-  
-  // const firstSecElements = main.querySelectorAll('.defaultOpacity');
-  // for (let tag of firstSecElements) {
-  // tag.classList.add('opacity');
-  // tag.classList.remove('defaultOpacity');
-  // tag.classList.add('transition');
   for (let i = 0; i < arr.length; i++) {
- if (arr[i].classList.contains('opacity')){
+ if (arr[i].classList.contains('opacity') && (arr[i].closest('.filter2'))){
   arr[i].classList.remove('opacity');
   arr[i].classList.add('defaultOpacity');
-  arr[i].classList.add('transition');
  }
  for (let i = 0; i < arr.length; i++) {
    if ((arr[i].classList.contains('defaultOpacity')) && (arr[i].closest('.filter1'))) {
@@ -36,13 +28,6 @@ btnSignUp.addEventListener('click', e => {
  }
 }
 }
-  // const secondSecElements =  main.querySelectorAll('.opacity');
-  // for (let tag of secondSecElements) {
-  //  tag.classList.add('defaultOpacity');
-  //  tag.classList.remove('opacity');
-  //  tag.classList.add('transition');
-  // }
-
 })
 
 btnSignIn.addEventListener('click', e => {
@@ -50,19 +35,45 @@ btnSignIn.addEventListener('click', e => {
   secondSecform2.style.transform = 'translateY(0%)';
   firstSecform1.style.transform = 'translateY(-100%)';
   secondSecform1.style.transform = 'translateY(-100%)';
-  const secondSecElements = main.querySelectorAll('.defaultOpacity');
-  for (let tag of secondSecElements) {
-   tag.classList.remove('defaultOpacity');
-   tag.classList.add('opacity');
-  }
-  const firstSecElements = main.querySelectorAll('.opacity');
-  for (let tag of firstSecElements) {
-    tag.classList.remove('opacity');
-    tag.classList.add('defaultOpacity');
-  }
+   for (let i = 0; i < arr.length; i++) {
+ if (arr[i].classList.contains('opacity') && (arr[i].closest('.filter1'))){
+  arr[i].classList.remove('opacity');
+  arr[i].classList.add('defaultOpacity');
+  
+ }
+ for (let i = 0; i < arr.length; i++) {
+   if ((arr[i].classList.contains('defaultOpacity')) && (arr[i].closest('.filter2'))) {
+  arr[i].classList.add('opacity');
+  arr[i].classList.remove('defaultOpacity');
+ }
+}
+}
 })
 
+let eyes = document.getElementsByClassName('eyes');
 
+
+for (let eye of eyes) {
+ eye.addEventListener('click', function() {
+  if (eye.classList.contains('fa-eye-slash')) {
+    eye.classList.remove('fa-eye-slash');
+    eye.classList.add('fa-eye')
+    
+  } else {
+    eye.classList.remove('fa-eye');
+    eye.classList.add('fa-eye-slash')
+  }
+  
+ })
+}
+
+let inputs = document.getElementsByTagName('input');
+
+for (let input of inputs) {
+ input.addEventListener('focus', function() {
+  console.log(input)
+ })
+}
 
 
 
